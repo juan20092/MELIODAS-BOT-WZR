bb4ab2bf56fa3aded0a879355b7ca8644e5dfc2a
𝘑𝘶𝘢𝘯𝘊𝘰𝘳𝘦𝘞𝘻 <jbolnos85@gmail.com>
2026-05-21 21:43:01 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+global.owner = [
+  ["573000000000", "Owner"]
+]
+
+global.botName = "prueba"
+global.author = "prueba"
+global.packname = "prueba"
+
+global.prefix = /^[./#!]/
+
+global.sessions = "./sessions"
