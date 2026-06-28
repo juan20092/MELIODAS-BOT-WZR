12042fb19c8ad8e0987c07636297c1a4991f205c
𝘑𝘶𝘢𝘯𝘊𝘰𝘳𝘦𝘞𝘻 <jbolnos85@gmail.com>
2026-06-28 01:09:56 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -169,6 +169,31 @@ async function filesInit() {
 
 await filesInit()
 
+await filesInit()
+
+async function updateFromGitHub() {
+  exec('git pull', async (err, stdout) => {
+    if (err) return
+
+    if (
+      stdout.includes('Updating') ||
+      stdout.includes('changed') ||
+      stdout.includes('Fast-forward')
+    ) {
+      console.log(`
+🔄 ACTUALIZACIÓN DESDE GITHUB
+
+${stdout}
+`)
+
+      await filesInit()
+      await global.reloadHandler()
+    }
+  })
+}
+
+setInterval(updateFromGitHub, 15000)
+
 const reloadPlugin = async (_, filename) => {
   if (!filename || !pluginFilter(filename)) return
   const dir = join(pluginFolder, filename)
