a71a34297eb61e6e19cbcc3cbf2a3e5ee2b5a08e
𝘑𝘶𝘢𝘯𝘊𝘰𝘳𝘦𝘞𝘻 <jbolnos85@gmail.com>
2026-07-02 02:35:57 -0500

    Actualizar encuesta.js

--- plugins/grupos/encuesta.js
+++ plugins/grupos/encuesta.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, args, text, command, isAdmin, isBotAdmin }) => {
   if (!text) return m.reply(
-    `✳️ Usa el comando así:\n\n` +
-    `*.encuesta Pregunta | opción1 | opción2 | opción3*`
+    `✳️ 𝚄𝚜𝚊 𝚎𝚕 𝚌𝚘𝚖𝚊𝚗𝚍𝚘 𝚊𝚜𝚒́:\n\n` +
+    `*.𝚎𝚗𝚌𝚞𝚎𝚜𝚝𝚊 𝙿𝚛𝚎𝚐𝚞𝚗𝚝𝚊 | 𝚘𝚙𝚌𝚒𝚘́𝚗𝟷 | 𝚘𝚙𝚌𝚒𝚘́𝚗𝟸 | 𝚘𝚙𝚌𝚒𝚘́𝚗𝟹*`
   )
 
   // Separar pregunta y opciones
@@ -9,15 +9,15 @@ let handler = async (m, { conn, args, text, command, isAdmin, isBotAdmin }) => {
 
   if (parts.length < 3)
     return m.reply(
-      `⚠️ Debes poner mínimo una pregunta y 2 opciones:\n\n` +
-      `*.encuesta ¿Pregunta? | opción1 | opción2*`
+      `⚠️ 𝙳𝚎𝚋𝚎𝚜 𝚙𝚘𝚗𝚎𝚛 𝚖𝚒́𝚗𝚒𝚖𝚘 𝚞𝚗𝚊 𝚙𝚛𝚎𝚐𝚞𝚗𝚝𝚊 𝚢 𝟸 𝚘𝚙𝚌𝚒𝚘𝚗𝚎𝚜:\n\n` +
+      `*.𝚎𝚗𝚌𝚞𝚎𝚜𝚝𝚊 ¿𝙿𝚛𝚎𝚐𝚞𝚗𝚝𝚊? | 𝚘𝚙𝚌𝚒𝚘́𝚗𝟷 | 𝚘𝚙𝚌𝚒𝚘́𝚗𝟸*`
     )
 
   let question = parts[0]
   let options = parts.slice(1)
 
   if (options.length > 12)
-    return m.reply('❌ Máximo 12 opciones permitidas.')
+    return m.reply('🚫 𝙼𝚊́𝚡𝚒𝚖𝚘 𝟷𝟸 𝚘𝚙𝚌𝚒𝚘𝚗𝚎𝚜 𝚙𝚎𝚛𝚖𝚒𝚝𝚒𝚍𝚊𝚜')
 
   await conn.sendMessage(m.chat, {
     poll: {
